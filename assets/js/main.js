0f130ffe46171a983a3fa9726cba79311fb3e533
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-01 14:59:51 +0100

    testing

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+console.log("test");
+
+
+/* 
+map()
+
+Variable upperDrinks
+        alle in Großbuchstaben
+
+
+        dann in ArrowFunction umwandeln
+*/
+
+
+let getraenke = [
+    "Coca-Cola",
+    "Apfelsaft",
+    "Pepsi",
+    "Traubensaft",
+    "Sprite",
+    "Orangensaft",
+    "Red Bull Energy Drink",
+    "Fanta"
+]
+
+
+// function getraenke
+function upperDrinks() {
+    getraenke.sort()
+    getraenke.map((element) => {
+        const body = document.querySelector("body")
+        body.innerHTML += `${element.toUpperCase()} --> </br> `
+        console.log(element.toUpperCase())
+    })
+
+
+}
+
+
+upperDrinks();
+
+// Arrow functions
+const newGetränke = getraenke.map((element) => {
+    const body = document.querySelector("body")
+    body.innerHTML += `${element.toUpperCase()},  `
+    console.log(element.toUpperCase())
+})
+
